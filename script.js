a2e735106e299cd066abd4fe7b38e7b89cd9632e
bruxellesflorian <florianbertchi@gmail.com>
2020-12-07 12:11:36 +0100

    horaire dynamique

---
+++ script.js
@@ -0,0 +1,36 @@
+const day = document.querySelector(".jour")
+
+let ladate = new Date()
+let h = ladate.getHours();
+if (h < 10) {
+    h = "0" + h
+}
+let m = ladate.getMinutes();
+if (m < 10) {
+    m = "0" + m
+}
+let s = ladate.getSeconds();
+if (s < 10) {
+    s = "0" + s
+}
+
+console.log('ladate.getDay():', ladate.getDay())
+
+day.addEventListener("mouseover", function () {
+    if (h < 11) {
+        day.style.background = " red"
+    } else if (h < 11 && m < 30) {
+        day.style.background = " red"
+    } else if (h == 11 && m > 30) {
+        day.style.background = " rgb(101, 196, 101"
+    } else if (h >= 12 && h <= 14) {
+        day.style.background = " rgb(101, 196, 101"
+    } else if (h >= 18) {
+        day.style.background = " rgb(101, 196, 101"
+    } else if (h >= 22 && m >= 30) {
+        day.style.background = " red"
+    }
+})
+day.addEventListener("mouseout", function () {
+    day.style.background = "white"
+})
